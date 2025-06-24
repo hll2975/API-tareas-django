13056462f32ff2cf7e02559afadff1126437a3cc
hll2975 <56049876+hll2975@users.noreply.github.com>
2025-06-24 10:29:39 -0300

    Add files via upload

---
+++ deepseek_bash_20250624_9f86e3.sh
@@ -0,0 +1,6 @@
+git init
+echo "venv/" >> .gitignore
+git add .
+git commit -m "Mi primera API REST"
+git remote add origin https://github.com/tuusuario/api-tareas-django.git
+git push -u origin main
